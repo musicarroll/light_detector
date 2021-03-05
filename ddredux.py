c614cf65fc50eafc1ebe1a7b851d5bdc0f139127
musicarroll <musicarroll@gmail.com>
2021-03-04 22:50:47 -0600

    Added new plots

--- ddredux.py
+++ ddredux.py
@@ -11,6 +11,8 @@ import numpy as np
 filename = sys.argv[1]
 #filename = '/home/mcarroll/Documents/python/light_detector/test.csv'
 df = pd.read_csv(filename, delimiter='\t')
+df['timestamp'] = df['timestamp']-df['timestamp'][0]
+
 
 if sys.argv[2]!='all':
         light_num = int(sys.argv[2])
@@ -28,7 +30,7 @@ for light_num in light_list:
 
 
         ax0 = plt.subplot(4,2,2*light_num+1)
-        plt.plot(myrows['value'])
+        plt.plot(myrows['timestamp'],myrows['value'])
         if light_num < len(light_list)-1 and len(light_list)>1:
                 plt.setp(ax0.get_xticklabels(), visible=False)
         if light_num==0:
@@ -71,23 +73,51 @@ for light_num in light_list:
         # print(local_minima)
         # print(local_minima.size)
 
-        lowpoint = lowpoints['value'].min()
-        lowpoint_index = lowpoints['value'].idxmin()
-        print('Light: ',light_num,' Min Value: ',lowpoint,' at index: ',lowpoint_index, ' at time: ',
+        lowpoint = local_minima['value'].min()
+        lowpoint_index = local_minima['value'].idxmin()
+        print('***********************************\nLight: ',light_num,\
+                ' Min Value: ',lowpoint,' at index: ',lowpoint_index, ' at time: ',\
                 df.iloc[lowpoint_index]['timestamp'],' num stds: ', (mymean-lowpoint)/mystd)
 
-        times = np.zeros((lowpoints.index.size,1))
+        times = np.zeros((local_minima.index.size,1))
         times = local_minima['timestamp']
         delta_times = np.diff(times)
+        print('Mean Delta Time: ',delta_times.mean(),' Stdev: ',delta_times.std())
+        print('Delta Times between Local Minima: \n ')
+        print(delta_times)
         # print(delta_times)
         # print(delta_times.mean())
 
         ax0 = plt.subplot(4,2,2*light_num+2)
-        if light_num < len(light_list)-1 and len(light_list)>1:
-                plt.setp(ax0.get_xticklabels(), visible=False)
+        # if light_num < len(light_list)-1 and len(light_list)>1:
+        #         plt.setp(ax0.get_xticklabels(), visible=False)
         if light_num==0:
                 plt.title('Local Minima: Delta Times for Light Crossings')
-        plt.plot(delta_times)
+        plt.plot(times[0:local_minima.index.size-1],delta_times)
 
 
 plt.show()
+
+
+plt.style.use('seaborn')
+plt.figure(2)
+
+for light_num in light_list:
+        filvar = (df['light']==light_num)
+        myrows = (df[filvar].head(5000))
+        alltimes = np.zeros((myrows.index.size,1))
+        alltimes = myrows['timestamp']
+        allvalues = np.zeros((myrows.index.size,1))
+        allvalues = myrows['value']
+        all_delta_times = np.diff(alltimes[100:])
+        print(all_delta_times.shape)
+        print('Mean Time Delta for Light ',light_num,': ',np.mean(all_delta_times))
+        print('Stdev Time Delta for Light ',light_num,': ',np.std(all_delta_times))
+
+        ax0 = plt.subplot(4,2,2*light_num+1)
+        plt.plot(alltimes[100:myrows.index.size-1],all_delta_times)
+        ax0 = plt.subplot(4,2,2*light_num+2)
+        plt.plot(alltimes,allvalues)
+
+plt.show()
+
